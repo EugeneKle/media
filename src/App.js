20b86ec885abff388ef5c34712e98f98b5205316
Eugene <klev.evzen@gmail.com>
2023-01-16 13:41:03 +0100

    initial redux template

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
-import "./index.css";
+import UsersList from "./components/UsersList";
 
 function App() {
-  return <div>App</div>;
+  return (
+    <div className="container mx-auto">
+      <UsersList />
+    </div>
+  );
 }
 
 export default App;
