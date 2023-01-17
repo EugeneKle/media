831413a635e16871c295b547288cf6b6b4625c86
Eugene <klev.evzen@gmail.com>
2023-01-17 19:31:08 +0100

    refactor tags and added albums list item

--- src/components/AlbumsList.js
+++ src/components/AlbumsList.js
@@ -1,37 +1,35 @@
 import { useFetchAlbumsQuery, useAddAlbumMutation } from "../store";
 import Skeleton from "./Skeleton";
-import ExpandablePanel from "./ExpandablePanel";
+
 import Button from "./Button";
+import AlbumsListItem from "./AlbumsListItem";
 
 function AlbumsList({ user }) {
-  const { data, error, isLoading } = useFetchAlbumsQuery(user);
-  const [addAlbum, result] = useAddAlbumMutation();
+  const { data, error, isFetching } = useFetchAlbumsQuery(user);
+  const [addAlbum, results] = useAddAlbumMutation();
 
   const handleAddAlbum = () => {
     addAlbum(user);
   };
 
   let content;
-  if (isLoading) {
-    content = <Skeleton times={3} />;
+  if (isFetching) {
+    content = <Skeleton className="h-10 w-full" times={3} />;
   } else if (error) {
     content = <div>Error loading albums.</div>;
   } else {
     content = data.map((album) => {
-      const header = <div>{album.title}</div>;
-      return (
-        <ExpandablePanel key={album.id} header={header}>
-          Albums for {user.name}
-        </ExpandablePanel>
-      );
+      return <AlbumsListItem key={album.id} album={album} />;
     });
   }
 
   return (
     <div>
-      <div>
-        Albums for {user.name}
-        <Button onClick={handleAddAlbum}>+ Add album</Button>
+      <div className="m-2 flex flex-row justify-between items-center">
+        <h3 className="text-lg font-bold">Albums for {user.name}</h3>
+        <Button onClick={handleAddAlbum} loading={results.isLoading}>
+          + Add album
+        </Button>
       </div>
       <div>{content}</div>
     </div>
--- src/store/thunks/fetchUsers.js
+++ src/store/thunks/fetchUsers.js
@@ -3,17 +3,8 @@ import axios from "axios";
 
 const fetchUsers = createAsyncThunk("users/fetch", async () => {
   const response = await axios.get("http://localhost:3005/users");
-  // dev only!!!
-  await pause(1000);
 
   return response.data;
 });
-
-// dev only!!!
-const pause = (duration) => {
-  return new Promise((resolve) => {
-    setTimeout(resolve, duration);
-  });
-};
 
 export { fetchUsers };
