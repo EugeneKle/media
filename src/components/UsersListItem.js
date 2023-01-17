6ffd7ac28d1f280caa8a9195676d20bf5f6c1379
Eugene <klev.evzen@gmail.com>
2023-01-17 12:30:22 +0100

    added expandable panel

--- src/components/UsersListItem.js
+++ src/components/UsersListItem.js
@@ -2,23 +2,28 @@ import { GoTrashcan } from "react-icons/go";
 import { removeUser } from "../store";
 import Button from "./Button";
 import useThunk from "../hooks/use-thunk";
+import ExpandablePanel from "./ExpandablePanel";
+import AlbumsList from "./AlbumsList";
 
 function UsersListItem({ user }) {
   const [doRemoveUser, isLoading, error] = useThunk(removeUser);
   const handleClick = () => {
     doRemoveUser(user);
   };
-
+  const header = (
+    <>
+      <Button className="mr-3" loading={isLoading} onClick={handleClick}>
+        <GoTrashcan />
+      </Button>
+      {error && <div>Error deleting user.</div>}
+      {user.name}
+    </>
+  );
+  
   return (
-    <div key={user.id} className="mb-2 border rounded">
-      <div className="flex p-2 justify-between item-center cursor-pointer">
-        <Button loading={isLoading} onClick={handleClick}>
-          <GoTrashcan />
-        </Button>
-        {error && <div>Error deleting user.</div>}
-        {user.name}
-      </div>
-    </div>
+    <ExpandablePanel header={header}>
+      <AlbumsList user={user} />
+    </ExpandablePanel>
   );
 }
 
