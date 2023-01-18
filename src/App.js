337b1ce90020a8dce20055dbbb08a8d01e1e1d4f
Eugene <klev.evzen@gmail.com>
2023-01-18 12:40:18 +0100

    set tags and add/remove new photos

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
+import { useEffect } from "react";
 import UsersList from "./components/UsersList";
 
 function App() {
+
+  useEffect(() => {
+    console.log('Hello')
+  }, [])
   return (
     <div className="container mx-auto">
       <UsersList />
